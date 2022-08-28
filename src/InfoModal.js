668a680c61281c86e89ddc2484a657ece9de1a86
Peyton Bechard <pbechard@my.yorku.ca>
2022-08-28 19:43:33 +0800

    Adds instructions to InfoModal component

--- src/InfoModal.js
+++ src/InfoModal.js
@@ -4,18 +4,20 @@ import './InfoModal.css';
 function InfoModal(props) {
   return (
     <div className='modal'>
-      <h2>Created by Peyton Bechard, 2022,</h2>
-      <h2>using Zachary Spielberger's awesome</h2>
-      <h2>
+      <h2>Instructions:</h2>
+      <p>In each level, you can only click on each character one time!</p>
+      <p></p>
+      <h4>
+        Created by Peyton Bechard, 2022, using Zachary Spielberger's awesome
         <a href='https://www.bobsburgersapi.com/' className='modal-link'>
           Bob's Burgers API
         </a>
-      </h2>
-      <h6>
+      </h4>
+      <h4>
         <a href='https://www.github.com/pmbechard' className='modal-link'>
           Check out the code and my other projects on GitHub.
         </a>
-      </h6>
+      </h4>
       <button onClick={props.info}>Close</button>
     </div>
   );
